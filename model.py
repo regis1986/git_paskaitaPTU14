dc475cd67357161d70f672f457b574588ece5253
Regis <r.petrauskui@gmail.com>
2023-08-18 10:12:28 +0300

    importai, class Order_

--- model.py
+++ model.py
@@ -12,6 +12,19 @@ class Customer(Base):
     name = Column(String)
     email = Column(String)
 
+class Order_(Base):
+    __tablename__ = 'order_'
+    id = Column(Integer, primary_key=True)
+    date_ = Column(DateTime)
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+    status_id = Column(Integer, ForeignKey('status.id'))
+    customers = relationship('Customer', back_populates='orders')
+    statuss = relationship('Status', back_populates='orders')
+    products_orders = relationship('Product_order', back_populates='orders')
+
+
+
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
+
